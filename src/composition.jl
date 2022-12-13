cb13c14ec4fc84f3e24fd510913ccef546a83c26
Bonan Zhu <zhubonan@outlook.com>
2022-12-13 16:03:14 +0000

    Updated the Composition handling

--- src/composition.jl
+++ src/composition.jl
@@ -2,16 +2,29 @@
 For handling compositions
 =#
 import Base
-export Composition, formula
+export Composition, formula, atomic_weight
 
 """
 Type representing a composition
+
+The Composition type supports index access:
+
+```julia-repl
+julia> comp = Composition(:CO2)
+Composition(:CO2)
+julia> comp[:C] 
+1
+```
+
+Internally, the speices and quantities are stored as two vectors, yet the interface
+mimics that of a `Dict{Sybmol,Float64}`.
+
 """
 struct Composition
     species::Vector{Symbol}
     counts::Vector{Float64}
     function Composition(species, counts)
-        length(species) != length(counts) && error("Size mismatch") 
+        length(species) != length(counts) && error("Size mismatch")
         sidx = sortperm(species)
         new(species[sidx], counts[sidx])
     end
@@ -23,7 +36,7 @@ end
 
 Return `Composition` from pairs.
 """
-function Composition(pairs::Pair{Symbol, T}...) where T
+function Composition(pairs::Pair{Symbol,T}...) where {T}
     x = Array{Symbol}(undef, length(pairs))
     y = Array{Float64}(undef, length(pairs))
     i = 1
@@ -42,6 +55,8 @@ function formula(t::Composition)
     for (s, c) in zip(t.species, t.counts)
         push!(args, s)
         if round(c) == c
+            # Omitted 1, e.g. C1O2 -> CO2
+            Int(c) == 1 && continue
             push!(args, Symbol(Int(c)))
         else
             push!(args, Symbol(c))
@@ -49,6 +64,8 @@ function formula(t::Composition)
     end
     Symbol(args...)
 end
+
+Base.string(t::Composition) = string(formula(t))
 
 formula(t::Cell) = formula(Composition(t))
 
@@ -68,26 +85,52 @@ function Composition(comp::Dict)
 end
 
 
-function Base.getindex(t::Composition, key::Symbol) 
+function Base.getindex(t::Composition, key::Symbol)
     idx = findfirst(x -> x == key, t.species)
-    isnothing(idx) && return 0.
+    isnothing(idx) && return 0.0
     t.counts[idx]
 end
 
-Base.haskey(t::Composition, key) = key in t.species
+function Base.setindex!(t::Composition, value, key::Symbol)
+    idx = findfirst(x -> x == key, t.species)
+    if isnothing(idx)
+        push!(t.species, key)
+        push!(t.counts, value)
+        #Sort the speices and indices
+        perm = sortperm(t.species)
+        news = t.species[perm]
+        newc = t.counts[perm]
+        t.species .= news
+        t.counts .= newc
+    else
+        t.counts[idx] = value
+    end
+end
 
+
+Base.haskey(t::Composition, key) = key in t.species
 Base.keys(t::Composition) = t.species
 Base.pairs(t::Composition) = [a => b for (a, b) in zip(t.species, t.counts)]
 Base.iterate(t::Composition) = Base.iterate(Base.pairs(t))
 Base.iterate(t::Composition, i) = Base.iterate(Base.pairs(t), i)
 Base.length(t::Composition) = Base.length(t.species)
-function Base.:(==)(t::Composition, t2::Composition) 
+function Base.:(==)(t::Composition, t2::Composition)
     (t.species == t2.species) && (t.counts == t2.counts)
 end
-function Base.isequal(t::Composition, t2::Composition) 
+function Base.isequal(t::Composition, t2::Composition)
     (t.species == t2.species) && (t.counts == t2.counts)
 end
 Base.reduce(t::Composition) = t / gcd(Int.(t.counts)...)
+
+"""
+    reduced_and_factor(t::Composition)
+
+Return reduced formula and reduction factor
+"""
+function reduced_and_factor(t::Composition)
+    n = nform(t)
+    t / n, n
+end
 
 nform(t::Composition) = gcd(Int.(t.counts))
 nform(t::Cell) = nform(Composition(t))
@@ -102,7 +145,7 @@ function Composition(cell::Cell)
     unique_sp = sort(unique(sp_array))
     num_atoms = Array{Int}(undef, size(unique_sp))
     for i in 1:length(unique_sp)
-       num_atoms[i] = count(x -> x == unique_sp[i], sp_array)
+        num_atoms[i] = count(x -> x == unique_sp[i], sp_array)
     end
     Composition(unique_sp, num_atoms)
 end
@@ -116,7 +159,7 @@ function Composition(string::AbstractString)
     Composition(parse_formula_with_bracket(string))
 end
 
-Composition(symbol::Symbol)  = Composition(string(symbol))
+Composition(symbol::Symbol) = Composition(string(symbol))
 
 """
     parse_formula(formula)
@@ -124,13 +167,13 @@ Composition(symbol::Symbol)  = Composition(string(symbol))
 Parse a formula into a dictionary.
 """
 function parse_formula(formula, factor=1.0)
-    reg = r"([A-Z][a-z]*)\s*([\.eE\d]*)"
-    output = Dict{Symbol, Float64}()
+    reg = r"([A-Z][a-z]*)\s*([\.\d]*)"
+    output = Dict{Symbol,Float64}()
     for match in eachmatch(reg, formula)
         num = 1.0
         match[2] != "" && (num = parse(Float64, match[2]))
-        x = get(output, Symbol(match[1]), 0.)
-        x +=  num * factor
+        x = get(output, Symbol(match[1]), 0.0)
+        x += num * factor
         output[Symbol(match[1])] = x
     end
     return output
@@ -154,7 +197,7 @@ function expand_bracket(formula)
         # Parse the contant inside the bracket
         inner = parse_formula(m[1], factor)
         expanded_sym = join(["$(sym)$(num)" for (sym, num) in inner])
-        formula = replace(formula, m.match=>expanded_sym)
+        formula = replace(formula, m.match => expanded_sym)
         return expand_bracket(formula)
     end
     return formula
@@ -171,7 +214,7 @@ function parse_formula_with_bracket(formula)
 end
 
 
-function Base.:+(a::Composition, b::Composition) 
+function Base.:+(a::Composition, b::Composition)
     all_keys = unique(vcat(keys(a), keys(b)))
     nums = zeros(Float64, length(all_keys))
     for (i, key) in enumerate(all_keys)
@@ -180,5 +223,33 @@ function Base.:+(a::Composition, b::Composition)
     Composition(all_keys, nums)
 end
 
+function Base.:-(a::Composition, b::Composition)
+    nums = copy(a.counts)
+    out = deepcopy(a)
+    for (i, key) in enumerate(b.species)
+        if key in a.species
+            out[key] = a[key] - b[key]
+        else
+            throw(ErrorException("$(a) does not contain  $(key)"))
+        end
+    end
+    out
+end
+
 Base.:*(a::Composition, b::Real) = Composition(a.species, a.counts .* b)
 Base.:/(a::Composition, b::Real) = Composition(a.species, a.counts ./ b)
+
+atomic_weight(comp::Composition) = sum(elements[key].atomic_mass * value for (key, value) in pairs(comp))
+
+function Base.contains(a::Composition, b::Composition)
+    akeys = keys(a)
+    for (s, v) in pairs(b)
+        if !(s in akeys)
+            return false
+        end
+        if a[s] < v
+            return false
+        end
+    end
+    return true
+end
--- test/test_composition.jl
+++ test/test_composition.jl
@@ -3,7 +3,7 @@ using CellBase: Composition, Cell, Lattice, formula
 
 
 @testset "Composition" begin
-    comp = Composition(:C => 1, :O=>2)
+    comp = Composition(:C => 1, :O => 2)
     @test comp[:C] == 1.0
     @test comp[:O] == 2.0
 
@@ -16,7 +16,7 @@ using CellBase: Composition, Cell, Lattice, formula
     @test comp[:O] == 8.0
     @test comp[:H] == 4.0
 
-    cell = Cell(Lattice(10, 10, 10), [1, 1], [[0., 0., 0.], [1., 1., 1.]])
+    cell = Cell(Lattice(10, 10, 10), [1, 1], [[0.0, 0.0, 0.0], [1.0, 1.0, 1.0]])
     comp = Composition(cell)
     @test comp[:H] == 2.0
 
@@ -36,10 +36,24 @@ using CellBase: Composition, Cell, Lattice, formula
     c1 = Composition("C3O4")
     @test formula(c1) == :C3O4
     show(devnull, c1)
-    
+
     # Equality test
     @test Composition(formula(c1)) == c1
 
     # Hash
     @test hash(Composition(:C10O10)) == hash(Composition(:C10O10))
+
+    # Settings index
+    c1 = Composition("C3O4")
+    c1[:C] = 2
+    @test c1 == Composition(:C2O4)
+    c1[:Zn] = 2
+    @test c1 == Composition(:C2O4Zn2)
+
+    @test contains(Composition(:C2O8), Composition(:CO))
+
+    @test Composition(:C2O2) - Composition(:O) == Composition(:C2O)
+
+    # Obtain atomic weight
+    CellBase.atomic_weight(c1)
 end
